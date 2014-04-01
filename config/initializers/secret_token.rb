74dcccedc771635f88d4f8f948da35998e6708f1
kyunga <kyunga@seedtime.co>
2014-04-01 12:48:58 +0900

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Repub::Application.config.secret_key_base = 'b5d1e4443aeeef17fe648d41e04ef202b2500161d5e02719c3a83702caa51b4132501f0dc0e7a9eaada7e24d535d5607521c53506eeb25d2119fcf92fa5d7f3b'
